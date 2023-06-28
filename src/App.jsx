1b7407b523987044b179157cee3af8fc9f238f58
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-06-28 23:28:37 +0200

    Fetched data from backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/category/:CatName" element={<Category />} />
+        <Route path="/category/:CatSlug" element={<Category />} />
+        <Route path="/product/:prodSlug" element={<Product />} />
       </Routes>
     </>
   );
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,26 +1,46 @@
-import ipodImg from "../assets/ipod.jpg";
+import prodImg from "../assets/ipod.jpg";
 
-const ProductItem = () => {
+const ProductItem = ({ prod_info }) => {
+  const ProdPrice = ({ price }) => {
+    const [intNum, setIntNum] = useState("");
+    const [centNum, setCentNum] = useState("");
+
+    useEffect(() => {
+      if (price) {
+        const priceArr = price.toString().split(".");
+        setIntNum(priceArr[0] ?? "00");
+        setCentNum(priceArr[1] ?? "00");
+      }
+    }, [price]);
+
+    return (
+      <>
+        <span className="align-top text-base">R</span>
+        {intNum}
+        <span className="align-top text-base">{centNum}</span>
+      </>
+    );
+  };
+  console.log(prod_info);
   return (
     <div className="first-col">
       <div className="relative rounded-xl overflow-hidden bg-gray-200 hover:w-200">
-        <img src={ipodImg} alt="Ipod-picture" />
+        <img
+          src={`${import.meta.env.VITE_API_URL}/image/${prod_info.prod_img}`}
+          alt={prod_info.prod_name}
+        />
         <div className="rounded-full absolute top-1 right-1 bg-white hover:bg-girlcode-pink w-10 h-10 text-center cursor-pointer">
           <span className="material-symbols-outlined leading-10">favorite</span>
         </div>
       </div>
       <div className="">
         <div className=" flex justify-between items-center">
-          <h3 className=" text-xl font-bold">HomePod Mini</h3>
+          <h3 className=" text-xl font-bold">{prod_info.prod_name}</h3>
           <div className="text-xl font-bold">
-            <span className="align-top text-base">R</span>
-            299
-            <span className="align-top text-base">.99</span>
+            {<ProdPrice price={prod_info.prod_price} />}
           </div>
         </div>
-        <p className="text-sm text-gray-900">
-          Table with air purifier, stained veneer/black
-        </p>
+        <p className="text-sm text-gray-900">{prod_info.prod_desc}</p>
         <div className="flex mb-2">
           <ul className="flex">
             <li>
